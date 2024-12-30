f339c9fff8d051b9727252e8e475ed68fae333b5
Rekansh <rekansh.patel@gmail.com>
2024-12-30 14:42:43 +0530

    removed document db functionality.

---
+++ CommonLibrary/DynamicJsonConverter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CommonLibrary
+{
+    /// <summary>
+    /// A custom JSON converter for dynamic properties.
+    /// </summary>
+    public class DynamicJsonConverter : JsonConverter<dynamic>
+    {
+        public override dynamic Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var jsonElement = JsonSerializer.Deserialize<JsonElement>(ref reader, options);
+            return ConvertJsonElementToDictionary(jsonElement);
+        }
+
+        public override void Write(Utf8JsonWriter writer, dynamic value, JsonSerializerOptions options)
+        {
+            JsonSerializer.Serialize(writer, value, options);
+        }
+
+        private static object ConvertJsonElementToDictionary(JsonElement element)
+        {
+            if (element.ValueKind == JsonValueKind.Object)
+            {
+                var dictionary = new Dictionary<string, object>();
+                foreach (var property in element.EnumerateObject())
+                {
+                    dictionary[property.Name] = ConvertJsonElementToDictionary(property.Value);
+                }
+                return dictionary;
+            }
+            else if (element.ValueKind == JsonValueKind.Array)
+            {
+                var list = new List<object>();
+                foreach (var item in element.EnumerateArray())
+                {
+                    list.Add(ConvertJsonElementToDictionary(item));
+                }
+                return list;
+            }
+            else
+            {
+                return element.ValueKind switch
+                {
+                    JsonValueKind.String => element.GetString(),
+                    JsonValueKind.Number => element.GetDouble(),
+                    JsonValueKind.True => true,
+                    JsonValueKind.False => false,
+                    JsonValueKind.Null => null,
+                    _ => element.GetRawText(),
+                };
+            }
+        }
+    }
+}
---
+++ CommonLibrary/JsonConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace CommonLibrary
+{
+    /// <summary>
+    /// Custom attribute to apply DynamicJsonConverter for dynamic properties.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class JsonConverter : JsonConverterAttribute
+    {
+        public JsonConverter() : base(typeof(DynamicJsonConverter))
+        {
+        }
+    }
+}
